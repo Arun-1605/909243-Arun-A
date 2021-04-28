9eb9850b583893fb1374012f5b8162e583e63fa0
Arun <83177915+Arun-1605@users.noreply.github.com>
2021-04-28 23:19:00 +0530

    MoviesWebAPI

---
+++ Web API/Handsons/MoviesWebAPI/MoviesWebAPI/Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MoviesWebAPI.Controllers
+{
+    public class MoviesController : ApiController
+    {
+        public string Get()
+        {
+            return "Hello From Web API";
+        }
+        public List<string> GetMovies()
+        {
+            return new List<string> { "Titanic", "Mission Impossible", "Matrix" };
+        }
+    }
+}
